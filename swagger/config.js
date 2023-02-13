6f2f0e7c29f08c5263e8c6c09fa6b7d006d9921d
CodeSandbox <support@codesandbox.io>
2023-02-13 15:52:09 +0000

    Initial commit

---
+++ swagger/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  openapi: "3.0.1",
+  info: {
+    version: "1.0.0",
+    title: "Invoices APIs Document",
+    description: "APIs document for all operations on the inboxes app.",
+    termsOfService: "",
+    contact: {
+      name: "Phathutshedzo Khabubu"
+    },
+    license: {
+      name: "MIT",
+      url: "https://opensource.org/licenses/MIT"
+    }
+  }
+};
